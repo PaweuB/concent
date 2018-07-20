76bd6ee6b19256567f9750f32c5c1f2cf1f4ce49
Radoslaw Wrzesien <radoslaw.wrzesien@codepoets.it>
2018-07-21 01:29:42 +0200

    Add Middleman Protocol base application

---
+++ middleman_protocol/README.md
@@ -0,0 +1,3 @@
+# middleman-protocol
+
+Shared module for formatting and parsing messages for SigningService and Middleman.
---
+++ middleman_protocol/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+
+setup(
+    name='Middleman-Protocol',
+    version='0.1',
+    url='',
+    maintainer='',
+    maintainer_email='',
+    packages=[
+        'middleman_protocol',
+    ],
+    package_data={},
+    python_requires='>=3.6',
+    install_requires=[],
+    tests_require=[],
+)
