d14afab95304ee50525da586c6a38910b67c9997
Radoslaw Wrzesien <radoslaw.wrzesien@codepoets.it>
2018-07-22 23:49:38 +0200

    fixup! Add Middleman Protocol base application

--- middleman_protocol/setup.py
+++ middleman_protocol/setup.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 
+import os
 from setuptools import setup
+
+
+def get_version():
+    with open(
+        os.path.join(
+            os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+            'RELEASE-VERSION',
+        ),
+        'r'
+    ) as version_file:
+        return version_file.read()
 
 
 setup(
     name='Middleman-Protocol',
-    version='0.1',
+    version=get_version(),
     url='',
     maintainer='',
     maintainer_email='',
